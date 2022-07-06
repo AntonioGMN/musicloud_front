64a501191c6785f44a7a422a8f553c9037ea32c2
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-06 16:48:48 -0300

    feat: using navigation component inside sidebar

--- src/layouts/SidebarLayout/SidebarLayout.jsx
+++ src/layouts/SidebarLayout/SidebarLayout.jsx
@@ -1,9 +1,14 @@
 import Logo from "../../components/Logo";
 import Profile from "../../components/Sidebar/Profile";
 
+import Navigation from "../../components/Sidebar/Navigation";
+
 export default () => (
 	<div className="flex flex-col items-center justify-between p-[10px] absolute h-[calc(100%-80px)] w-[200px] bg-dark text-white">
 		<Logo />
+
+    <Navigation />
+
 		<Profile />
 	</div>
 )
