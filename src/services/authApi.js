441812170be523190ba7fc8e9c1c26eb8e5e40d7
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-06 12:31:08 -0300

    fix: wrong route for sign-in

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export async function signIn(email, password) {
-  const response = await api.post("/auth/sign-in", { email, password });
+  const response = await api.post("/auth/login", { email, password });
   return response.data;
 }
-//
+
