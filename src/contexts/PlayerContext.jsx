9beb087e3ca3580bf7b9f554007e9f076d8d9940
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-06 15:06:49 -0300

    feat: PlayerContext to manipulate current song

---
+++ src/contexts/PlayerContext.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect, useRef, createContext } from "react";
+
+import useSong from "../hooks/api/useSong";
+import { useToken } from "../hooks/useToken";
+
+const PlayerContext = createContext();
+export default PlayerContext;
+
+export function PlayerProvider({ children }) {
+  const [songId, setSongId] = useState(null);
+  const { song, loadSong, songLoading } = useSong();
+  const token = useToken();
+
+  useEffect(() => {
+    if (songId !== null) {
+      loadSong(songId, token);
+    }
+  }, [songId]);
+  
+  return (
+    <PlayerContext.Provider value={{ song, songId, setSongId, songLoading }}>
+      {children}
+    </PlayerContext.Provider>
+  );
+}
