f2d1cfc8c807f13fd75922e3e10c5512870162fc
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-06 17:09:46 -0300

    fix: song list alignment

--- src/components/Dashboard/Song.jsx
+++ src/components/Dashboard/Song.jsx
@@ -1,7 +1,7 @@
 import SongButton from "./SongButton";
 
 export default ({ children }) => (
-	<div className="flex justify-start items-center h-[40px] w-fit max-w-[840px] min-w-[120px] pr-[7px] ml-[40px] mb-[12px] border-1 border-light border-solid border-b rounded-none ">
+	<div className="flex justify-start items-center h-[40px] w-fit max-w-[840px] min-w-[120px] pr-[7px] ml-[40px] mb-[12px] border-1 pb-3 border-light border-solid border-b rounded-none ">
 		<SongButton playing={false}/>
 		<p className="text-[16px]">{children.title}</p>
 	</div>
